0d2e4a45e0a4c08d58d6ea3e7586431ece439f1e
AleksandrBashkaev <Bashkaev_a@mail.ru>
2023-06-14 18:38:50 +0400

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void amountIsGreaterBoundary () { //Тест, если сумма превышает границу
+        CashbackHackService service = new CashbackHackService ();
+
+        Assertions.assertEquals(0, service.remain(1500));
+    }
+
+    @Test
+    public void amountIsEqualBoundary () { //сумма равна границе
+        CashbackHackService service = new CashbackHackService ();
+
+        Assertions.assertEquals(0,service.remain(1000));
+    }
+    @Test
+    public void amountIsLessBoundary (){ // сумма меньше границы
+        CashbackHackService service = new CashbackHackService ();
+
+        Assertions.assertEquals(200, service.remain(800));
+
+    }
+
+}
